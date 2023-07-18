0d31ccce3d09b1f0f7bb598c2a18ba1c167fc083
Belfegorrr <pelichek.iago@gmail.com>
2023-07-18 15:01:38 -0300

    estilizar pagina home

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from 'styled-components'
+import { cores } from '../../styles'
+
+export const HeaderBar = styled.header`
+  background-color: ${cores.corHeader};
+  padding: 24px;
+`
--- src/styles.ts
+++ src/styles.ts
@@ -1,12 +1,27 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
-const Cores = {}
-
+export const cores = {
+  branco: '#000',
+  corHeader: '#e66767',
+  corBody: '#ffebd9',
+  corDestaque: '#fff8f1'
+}
 export const GlobalCss = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: Roboto, sans-serif;
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Roboto, sans-serif;
+}
+
+body {
+  background-color: ${cores.corBody};
+  color: ${cores.branco};
+}
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
 `
