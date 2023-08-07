793f8f8bd31fe97c17f840201c01854aa23f3532
Belfegorrr <pelichek.iago@gmail.com>
2023-08-06 22:28:33 -0300

    estilizaçao bamer

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,16 +1,36 @@
 import { styled } from 'styled-components'
+import { cores } from '../../styles'
+import { TagContainer } from '../Tag/styles'
 
 export const Imagem = styled.div`
   width: 100%;
   height: 300px;
-  display: block;
+
   background-size: cover;
   font-weigth: bold;
-  padding-top: 340px;
+  padding-top: 50px;
+
+  .container {
+    position: relative;
+    padding-top
+  }
+
+  ${TagContainer} {
+    position: absolute;
+    top: 32px;
+  }
 `
 
 export const Titulos = styled.h2`
-  position: center;
+  display: flex;
+  justify-content: center;
   font-size: 36px;
-  font-weight: bold;
+
+  padding-top: 90px;
+  color: ${cores.corDestaque};
+`
+
+export const HeaderBar = styled.header`
+  display: flex;
+  justify-content: space-evenly;
 `
