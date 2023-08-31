89a7a0045f7e011cb99cd19bf270f4c3ee943f60
Belfegorrr <pelichek.iago@gmail.com>
2023-08-31 16:58:38 -0300

    feat Modal

--- src/components/Pedidos/index.tsx
+++ src/components/Pedidos/index.tsx
@@ -1,5 +1,16 @@
 import bgpedido from '../../assets/images/image 3.png'
-import { BotaoMenu, CardPedido, DescricaoMenu, TituloMenu } from './styles'
+import close from '../../assets/images/close.svg'
+import {
+  BotaoMenu,
+  CardPedido,
+  DescricaoMenu,
+  Modal,
+  ModalBotao,
+  ModalContent,
+  ModalImagem,
+  ModalPedido,
+  TituloMenu
+} from './styles'
 
 const Pedidos = () => (
   <div className="container">
@@ -12,6 +23,33 @@ const Pedidos = () => (
       </DescricaoMenu>
       <BotaoMenu>Adicionar ao carrinho</BotaoMenu>
     </CardPedido>
+    <Modal>
+      <ModalContent>
+        <div>
+          <ModalImagem src={bgpedido} alt="Imagem do pedido" />
+        </div>
+        <ModalPedido>
+          <img src={close} alt="Close Icon" />
+          <h2>Pizza Marguerita</h2>
+          <p>
+            A pizza Margherita é uma pizza clássica da culinária italiana,
+            reconhecida por sua simplicidade e sabor inigualável. Ela é feita
+            com uma base de massa fina e crocante, coberta com molho de tomate
+            fresco, queijo mussarela de alta qualidade, manjericão fresco e
+            azeite de oliva extra-virgem. A combinação de sabores é perfeita,
+            com o molho de tomate suculento e ligeiramente ácido, o queijo
+            derretido e cremoso e as folhas de manjericão frescas, que adicionam
+            um toque de sabor herbáceo. É uma pizza simples, mas deliciosa, que
+            agrada a todos os paladares e é uma ótima opção para qualquer
+            ocasião.
+            <br />
+            <br />
+            Serve: de 2 a 3 pessoas
+          </p>
+          <ModalBotao>Adicionar ao carrinho - R$ 60,90</ModalBotao>
+        </ModalPedido>
+      </ModalContent>
+    </Modal>
   </div>
 )
 
--- src/components/Pedidos/styles.ts
+++ src/components/Pedidos/styles.ts
@@ -40,3 +40,65 @@ export const TituloMenu = styled.h3`
   margin: 10px 0 8px 8px;
   color: ${cores.corBody};
 `
+
+/*-------------------------- */
+
+export const Modal = styled.div`
+  position: fixed;
+  top: 100px;
+  left: 300px;
+  width: 100%;
+  height: 100%;
+`
+export const ModalContent = styled.div`
+  width: 1024px;
+  height: 344px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: ${cores.corHeader};
+  color: ${cores.branco};
+`
+export const ModalPedido = styled.div`
+  position: relative;
+  padding: 20px;
+
+  img {
+    position: absolute;
+    top: 0;
+    right: 25px;
+  }
+
+  h2 {
+    font-size: 18px;
+    font-weight: 900;
+    margin-bottom: 30px;
+  }
+
+  p {
+    line-height: 22px;
+    font-size: 14px;
+    font-weight: 400;
+    margin-bottom: 10px;
+  }
+`
+export const ModalImagem = styled.img`
+  margin-left: 30px;
+  width: 280px;
+  height: 280px;
+  top: 0;
+  right: 0;
+`
+export const ModalBotao = styled.button`
+  border: none;
+  width: 218px;
+  height: 24px;
+  padding: 2px;
+
+  font-weight: 700;
+  font-size: 14px;
+
+  background-color: ${cores.corBody};
+  color: ${cores.corHeader};
+`
