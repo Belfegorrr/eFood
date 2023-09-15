b1bc5c0c059da317e3d44499f9f7bcd235df9508
Belfegorrr <pelichek.iago@gmail.com>
2023-09-14 22:28:36 -0300

    feat: Implementando Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import { GlobalCss } from './styles'
 import Rotas from './routes'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="container"></div>
-      <Rotas />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="container"></div>
+        <Rotas />
+      </BrowserRouter>
+    </Provider>
   )
 }
 export default App
